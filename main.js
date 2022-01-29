b015faf76e91a88e72402013daea72183ff8ebe5
Sakethram17 <90086812+Sakethram17@users.noreply.github.com>
2022-01-29 21:50:40 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,14 @@
+function peload(){
+}
+
+function setup(){
+    canvas = createCanvas(300, 300);
+    canvas.center();
+}
+
+function draw(){
+}
+
+function take_snapshot(){
+    save('myFilterImage.png');
+}
